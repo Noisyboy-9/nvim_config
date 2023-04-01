d73b4c9de6c64762d10af56d32289758f80d4829
sina shariati <sina.shariati@yahoo.com>
2023-04-01 15:08:23 +0330

    Add search for objects keymap to telescope keymaps

--- lua/noisyboy/core/keymaps.lua
+++ lua/noisyboy/core/keymaps.lua
@@ -2,8 +2,7 @@ vim.g.mapleader = ","
 local keymap = vim.keymap
 
 -- general keymaps
-keymap.set("i", "jk", "<ESC>")
-keymap.set("n", "<leader>nh", ":nohlsearch<CR>")
+keymap.set("n", "<leader>nh", "<cmd>nohlsearch<CR>")
 keymap.set("n", "x", '"_x')
 
 
@@ -11,34 +10,35 @@ keymap.set("n", "x", '"_x')
 keymap.set("n", "<leader>sv", "<C-w>v") -- split verical
 keymap.set("n", "<leader>sh", "<C-w>s") -- spilt horizontal
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal size
-keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
+keymap.set("n", "<leader>sx", "<cmd>close<CR>") -- close split window
 keymap.set("n", "<leader>h", "<C-w>h") -- go to left split window
 keymap.set("n", "<leader>l", "<C-w>l") -- go to right split window
 
 
 
 -- manage tabs
-keymap.set("n", "<leader>to", ":tabnew<CR>")
-keymap.set("n", "<leader>tx", ":tabclose<CR>")
-keymap.set("n", "<leader>tn", ":tabn<CR>")
-keymap.set("n", "<leader>tp", ":tabp<CR>")
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>")
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")
 
 -- jumping to files
-keymap.set("n", "<leader>ev", ":tabnew ~/.config/nvim/init.lua<CR>")
-keymap.set("n", "<leader>pack", ":tabnew ~/.config/nvim/lua/noisyboy/plugins-setup.lua<CR>")
-keymap.set("n", "<leader>key", ":tabnew ~/.config/nvim/lua/noisyboy/core/keymaps.lua<CR>")
+keymap.set("n", "<leader>ev", "<cmd>tabnew ~/.config/nvim/init.lua<CR>")
+keymap.set("n", "<leader>pack", "<cmd>tabnew ~/.config/nvim/lua/noisyboy/plugins-setup.lua<CR>")
+keymap.set("n", "<leader>key", "<cmd>tabnew ~/.config/nvim/lua/noisyboy/core/keymaps.lua<CR>")
 
 -- nvim-tree
-keymap.set("n", "<leader>pe", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>pe", "<cmd>NvimTreeToggle<CR>")
 
 
 -- telescope 
-keymap.set("n", "<leader>ff", ":Telescope find_files<CR>") -- fuzzy find files
-keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>") -- search all project for pattern
-keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>") -- search file for pattern
-keymap.set("n", "<leader>fe", ":Telescope buffers<CR>") -- recently opened file
-keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>")
-keymap.set("n", "<leader>fp", ":Telescope workspaces<CR>")
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>") -- fuzzy find files
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>") -- search all project for pattern
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>") -- search file for pattern
+keymap.set("n", "<leader>fe", "<cmd>Telescope buffers<CR>") -- recently opened file
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
+keymap.set("n", "<leader>fp", "<cmd>Telescope workspaces<CR>")
+keymap.set("n", "<leader>fo", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
 
 
 -- lsp keymaps 
