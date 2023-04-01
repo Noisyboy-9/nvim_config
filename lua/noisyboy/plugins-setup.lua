49d5c158d5e84c2d160917153a8240df1a8fe7c9
sina shariati <sina.shariati@yahoo.com>
2023-04-01 14:22:14 +0330

    Add lazygit to packer

--- lua/noisyboy/plugins-setup.lua
+++ lua/noisyboy/plugins-setup.lua
@@ -42,7 +42,8 @@ return packer.startup({function(use)
     use 'numToStr/Comment.nvim'
     use 'kwkarlwang/bufjump.nvim'
     use 'nanozuki/tabby.nvim'
-    use {"akinsho/toggleterm.nvim", tag = '*'}
+    use {'akinsho/toggleterm.nvim', tag = '*'}
+    use {'kdheepak/lazygit.nvim'}
 
 
 
@@ -91,7 +92,6 @@ return packer.startup({function(use)
 
     -- golang 
     use 'fatih/vim-go'
-
 
     if packer_bootstrap then
         require('packer').sync()
