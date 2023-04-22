f0a0053d17182bf62b5d4b01a62cb4ae4f938c5d
sina shariati <sina.shariati@yahoo.com>
2023-04-22 16:00:29 +0330

    Add help to tree-sitter

--- lua/noisyboy/plugins/tree-sitter.lua
+++ lua/noisyboy/plugins/tree-sitter.lua
@@ -5,11 +5,11 @@ if not ok then
 end
 
 tree_sitter.setup({
-    ensure_installed = {"lua", "rust", "toml", "go" , "c", "cpp"},
+    ensure_installed = {"help", "lua", "rust", "toml", "go" , "c", "cpp"},
     auto_install = true,
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting=false,
+        additional_vim_regex_highlighting = false,
     },
     indent = {enable = true},
     rainbow = {
