401c795ccbb2b00bf6e7177ad02c2edfa3115f84
sina shariati <sina.shariati@yahoo.com>
2023-04-22 15:04:23 +0330

    Add search git files and search for text

--- lua/noisyboy/core/keymaps.lua
+++ lua/noisyboy/core/keymaps.lua
@@ -36,12 +36,15 @@ keymap.set("n", "<leader>pe", "<cmd>NvimTreeToggle<CR>")
 
 -- telescope 
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>") -- fuzzy find files
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>") -- search all project for pattern
+keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<CR>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>") -- search file for pattern
 keymap.set("n", "<leader>fe", "<cmd>Telescope buffers<CR>") -- recently opened file
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
 keymap.set("n", "<leader>fp", "<cmd>Telescope workspaces<CR>")
 keymap.set("n", "<leader>fo", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
+keymap.set("n", "<leader>fs", function ()
+    require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ")})
+end) -- search all project for pattern
 
 
 -- code folding 
