e38993bccb4aa9c2e9f7937f2c03b048b590e481
sina shariati <sina.shariati@yahoo.com>
2023-04-22 15:54:16 +0330

    setup transparent config and add it to list of imports

--- init.lua
+++ init.lua
@@ -24,6 +24,7 @@ require("noisyboy.plugins.gitsigns")
 require("noisyboy.plugins.formatter")
 require("noisyboy.plugins.go")
 require("noisyboy.plugins.luasnip")
+require("noisyboy.plugins.transparent")
 
 -- lsp related plugins
 require("noisyboy.plugins.lsp.mason")
