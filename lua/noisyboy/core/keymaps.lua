b081d3a5c8d02bf52efedf0ec4f354a7faa3df2d
sina shariati <sina.shariati@snapp.cab>
2023-05-02 17:34:02 +0330

    Add keyamps for fold all and open all

--- lua/noisyboy/core/keymaps.lua
+++ lua/noisyboy/core/keymaps.lua
@@ -7,20 +7,20 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- source settings
-keymap.set("n","<leader>ss", "<cmd>so ~/.config/nvim/init.lua<cr>", {silent = false})
+keymap.set("n", "<leader>ss", "<cmd>so ~/.config/nvim/init.lua<cr>", { silent = false })
 
 -- general keymaps
 keymap.set("n", "<leader>nh", "<cmd>nohlsearch<CR>")
 keymap.set("n", "x", '"_x')
 
 
--- manage splits 
-keymap.set("n", "<leader>sv", "<C-w>v") -- split verical
-keymap.set("n", "<leader>sh", "<C-w>s") -- spilt horizontal
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal size
+-- manage splits
+keymap.set("n", "<leader>sv", "<C-w>v")         -- split verical
+keymap.set("n", "<leader>sh", "<C-w>s")         -- spilt horizontal
+keymap.set("n", "<leader>se", "<C-w>=")         -- make split windows equal size
 keymap.set("n", "<leader>sx", "<cmd>close<CR>") -- close split window
-keymap.set("n", "<leader>h", "<C-w>h") -- go to left split window
-keymap.set("n", "<leader>l", "<C-w>l") -- go to right split window
+keymap.set("n", "<leader>h", "<C-w>h")          -- go to left split window
+keymap.set("n", "<leader>l", "<C-w>l")          -- go to right split window
 
 
 
@@ -39,36 +39,36 @@ keymap.set("n", "<leader>key", "<cmd>tabnew ~/.config/nvim/lua/noisyboy/core/key
 keymap.set("n", "<leader>pe", "<cmd>NvimTreeToggle<CR>")
 
 
--- telescope 
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>") -- fuzzy find files
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")  -- fuzzy find files
 keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<CR>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>") -- search file for pattern
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
 keymap.set("n", "<leader>fp", "<cmd>Telescope workspaces<CR>")
 keymap.set("n", "<leader>fo", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>")
-keymap.set("n", "<leader>fs", function ()
-    require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ")})
+keymap.set("n", "<leader>fs", function()
+    require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
 end) -- search all project for pattern
 keymap.set("n", "<leader>u", "<cmd>Telescope undo<CR>")
 
 
--- code folding 
+-- code folding
 local ok, ufo = pcall(require, "ufo")
 if not ok then
-   print("can't require ufo")
+    print("can't require ufo")
     return
 end
-keymap.set("n", "zR", ufo.openAllFolds)
-keymap.set("n", "zM", ufo.closeAllFolds)
+vim.keymap.set('n', 'zO', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zC', require('ufo').closeAllFolds)
 
 -- trouble
-local remapOpts = {silent = true, noremap = true}
+local remapOpts = { silent = true, noremap = true }
 keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", remapOpts)
 keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", remapOpts)
 keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", remapOpts)
 keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", remapOpts)
 keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", remapOpts)
-keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",remapOpts)
+keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", remapOpts)
 
 --  lazygit
 keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<cr>")
@@ -77,7 +77,7 @@ keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<cr>")
 -- harpoon
 local ok, harpoon = pcall(require, "harpoon")
 if not ok then
-   print("can't require harpoon from keymaps file")
+    print("can't require harpoon from keymaps file")
     return
 end
 local mark = require("harpoon.mark")
