a1320efa7f06e5895b9edba896dd61db2e860f2a
sina shariati <sina.shariati@yahoo.com>
2023-09-05 21:13:47 +0330

    Remvoe nvim-cmp.lua and move necessary configs into lsp-config.lua

--- lua/noisyboy/plugins/lsp/lsp-config.lua
+++ lua/noisyboy/plugins/lsp/lsp-config.lua
@@ -1,7 +1,22 @@
 local lsp = require("lsp-zero")
 local lspkind = require("lspkind")
+local lspconfig = require("lspconfig")
+local cmp = require('cmp')
+local cmp_action = require("lsp-zero").cmp_action()
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
+vim.api.nvim_set_option('updatetime', 300)
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
 
 lsp.preset("recommended")
+lsp.setup()
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua_ls', {
@@ -14,55 +29,42 @@ lsp.configure('lua_ls', {
     }
 })
 
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-})
-
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    local keymap = vim.keymap
-    keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-    keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    lsp.default_keymaps({ buffer = bufnr })
 end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
-})
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
 
 cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- use enter key to confirm completion
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),      -- use cntrl+f to jump forward in luasnip
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),     -- use cntrl+b to jump backward in luasnip
+    }),
+
     formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
         format = lspkind.cmp_format({
-            mode = 'symbol',       -- show only symbol annotations
-            maxwidth = 50,         -- prevent the popup from showing more than provided characters
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-        })
-    }
+            maxwidth = 50,
+            ellipsis_char = "...",
+        }),
+    },
+})
+
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
+    }),
 })
